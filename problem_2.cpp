68a238e2b9b6d8be36348654c084d9f46a400504
Zia Rehman <iamziarehman@gmail.com>
2025-03-31 22:42:40 +0100

    Problem 2: Minor improvements

--- problem_2.cpp
+++ problem_2.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 
 int main() {
-  int limit = 4'000'000;
+  constexpr int limit = 4'000'000;
 
-  int a = 0;
-  int b = 1;
-  int sum = 0;
+  int a = 0, b = 1, sum = 0;
 
   while (b < limit) {
     if (b % 2 == 0) {
@@ -17,6 +15,6 @@ int main() {
     b = next;
   }
 
-  cout << sum << endl;
+  cout << sum << "\n";
   return 0;
 }
